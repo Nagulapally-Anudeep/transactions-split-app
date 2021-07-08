3fddcf552d3b9a4f88839e49d7426a6edad9c466
Nagulapally-Anudeep <nanudeep408@gmail.com>
2021-07-08 11:33:17 +0530

    created the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import Header from "./components/Header";
+import Addpayments from "./components/Addpayments";
+import List from "./components/List";
+import {split} from "./logic/split";
 
-function App() {
+export default function App() {
+  const [transactions,setTransactions] = useState([]);
+  const [empty,setEmpty] = useState(true);
+  const [event,setEvent] = useState("Before");
+
+  function addTransaction(transaction){
+    setTransactions(prevTransactions =>{
+       return [...prevTransactions,transaction];
+    });
+    setEmpty(false);
+  }
+
+  function handleClear(){
+    setTransactions([]);
+    setEmpty(true);
+    setEvent("Before");
+  }
+
+  function handleSplit(){
+    //console.log(transactions);
+    const output = split(transactions);
+    setEvent("After");
+    setTransactions(output);
+    //console.log(output);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Addpayments onAdd={addTransaction}/>
+      <div className="transaction-area">
+        <div className="details">
+          {empty? <p>No Transactions Available...</p> :
+            (<ul>
+              {transactions.map((transaction,id) =>{
+                 return <List 
+                     key={id} 
+                     event={event}
+                     payer={transaction.payer} 
+                     payee={transaction.payee} 
+                     amount={transaction.amount}/>;
+                    })
+              }
+          </ul>)
+          }
+        </div>
+        {!empty && <div className="buttons">
+             <button className="clear-button" onClick={handleClear}>Clear</button>
+             <button className="simplify" onClick={handleSplit}>Split</button>
+        </div> }
+      </div>
     </div>
   );
 }
 
-export default App;
+
